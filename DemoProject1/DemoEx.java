a056012b8306e87f7e3ba6e0229a632250bb8b69
Dommara Haritha <dommara.haritha@happiestminds.com>
2021-12-27 10:36:04 +0530

    This is my first commit

---
+++ DemoProject1/DemoEx.java
@@ -0,0 +1,13 @@
+package com.hm.java11.day10;
+
+import javax.swing.JOptionPane;
+
+public class DemoEx {
+
+	public static void main(String[] args) {
+		
+		String name= JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hai "+name);
+	}
+
+}
